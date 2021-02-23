60c5611db9490714cdbef65d7672a5431090065f
apitest <debian@debian10.linuxvmimages.local>
2021-02-23 01:08:24 -0600

    updated

--- app.py
+++ app.py
@@ -26,7 +26,4 @@ def create_app():
 
 if __name__ == "__main__":
    app = create_app()
-   app.run(host='0.0.0.0', debug=True)</td>
-  </tr>
-  <tr>
-    <td>
+   app.run(host='0.0.0.0', debug=True)
